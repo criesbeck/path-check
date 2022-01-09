61e73ab3d223c9596c78974a056a406c09a8645e
Chris Riesbeck <chris.riesbeck@gmail.com>
2022-01-09 17:07:11 -0600

    on windows, no bin subfolder

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const prefix = require('global-prefix');
+const isWin = process.platform === 'win32';
 
 if (!prefix) {
   console.log('NPM prefix not found');
   return -1;
 }
 
-const binPrefix = `${prefix}${path.sep}bin`;
+const binPrefix = isWin ? prefix : `${prefix}${path.sep}bin`;
 const paths = process.env.PATH.split(path.delimiter);
 
 if (paths.includes(binPrefix)) {
